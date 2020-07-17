2179b38221a4763b9529c6eacccdc759e401f01a
Jason Prasad <jasonprasad@stockx.com>
2020-07-17 13:55:13 -0400

    Adds reddit json request

--- APIStarterTests/APIStarterTests.swift
+++ APIStarterTests/APIStarterTests.swift
@@ -11,24 +11,19 @@ import XCTest
 
 class APIStarterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let session = URLSession(configuration: .default)
+        let url = URL(string: "https://www.reddit.com/.json")!
+        let expect = expectation(description: "dataTask completes")
+        
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            print("bar")
+            expect.fulfill()
         }
+        dataTask.resume()
+        print("foo")
+        
+        wait(for: [expect], timeout: 1.0)
     }
 
 }
